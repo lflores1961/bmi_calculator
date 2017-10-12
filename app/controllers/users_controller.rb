e6708f9861bda023f1c16f4a7c963c933e4f5834
Fernando Flores <fernando.flores1961@gmail.com>
2017-10-12 17:09:46 -0500

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome #{@user.name} to the BMI calculator"
       redirect_to @user
     else
