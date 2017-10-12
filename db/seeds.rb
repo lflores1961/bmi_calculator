dfda9f68bdbc642a1b5d3abad6379e4b58e47cb2
Fernando Flores <fernando.flores1961@gmail.com>
2017-10-12 11:23:39 -0500

    add database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Fernando Flores", email: "fernando.flores1961@gmail.com", password: "foobar",
+            password_confirmation: "foobar")
